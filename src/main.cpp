8a1f406fc4e9ee18e50e7066fbd68cfcdcd20d88
Mael MAINCHAIN <mael@mainchain.fr>
2018-10-04 15:36:06 +0200

    Adding serial port test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
+#define BAUD 9600
 
 void setup() {
     // put your setup code here, to run once:
+    Serial.begin(BAUD);
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
+    Serial.println("Hello From Arduino");
+    delay(5000);
 }
