bb293f20dc926ba6e520181a450d10c826bf62d3
Chloe <44chloeguilbaud@gmail.com>
2018-10-04 15:38:55 +0200

    feat: Communication avec votre ordinateur

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
+#define BAUD 9600
 
 void setup() {
     // put your setup code here, to run once:
+    Serial.begin(BAUD); // Définition du port de sortie 
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
+    Serial.println("Hello From Arduino");
+    delay(5000);
 }
