4779f71f89493630d0bc68f4affa39daa199cf12
Mael MAINCHAIN <mael@mainchain.fr>
2018-11-06 10:23:14 +0100

    TP3 Q2

--- src/main.cpp
+++ src/main.cpp
@@ -2,37 +2,70 @@
 #include <Bouton.h>
 #include "LowPower.h"
 #include <RH_RF95.h>
+#include <Wire.h>
 #include <T2WhisperNode.h>
+#include <LoRa.h>
 
 #define BAUD 9600
 
 // Element pins
 const int blueButton = 4;
-const int yellowButton = 5;
+// const int yellowButton = 5;
 
 int countD = 0;
-int countG = 0;
+// int countG = 0;
 
-/*Bouton boutonD(blueButton);
-Bouton boutonG(yellowButton);*/
+Bouton boutonD(blueButton);
+// Bouton boutonG(yellowButton);
 
-T2Flash myflash;
-RH_RF95 myradio;
+// T2Flash myflash;
+// RH_RF95 myradio;
+
+T2Message t2message;
 
 void setup() {
 
     // put your setup code here, to run once:
     Serial.begin(BAUD); // Définition du port de sortie 
 
+    Wire.begin();
     /** TP2 **/
-    
+
     // Pour mémoire flash
-    myflash.init(T2_WPN_FLASH_SPI_CS);
-    myflash.powerDown();
+    // myflash.init(T2_WPN_FLASH_SPI_CS);
+    // myflash.powerDown();
 
     // Pour puce LoRa
-    myradio.init();
-    myradio.sleep();
+    // myradio.init();
+    // myradio.sleep();
+
+    LoRa.setPins(10,7,2); 
+    if(LoRa.begin(868E6)) { 
+        Serial.println("Starting LoRa failed!"); 
+        while(1);
+    } else {
+        Serial.println("Starting");
+    }
+}
+
+void initialisation() {
+    Serial.println("Init");
+    LoRa.setPins(10,7,2); 
+    if(LoRa.begin(868E6)) { 
+        Serial.println("Starting LoRa failed!"); 
+        while(1);
+    }
+}
+
+void createChannel() {
+    Serial.println("create channel");
+}
+
+void emitData() {
+    Serial.println("emit data");
+    LoRa.beginPacket(); 
+    // LoRa.write(message, 10); 
+    LoRa.endPacket();
 }
 
 void loop() {
@@ -89,6 +122,23 @@ void loop() {
     // delay(5000);
 
     /** TP3 **/
+    // Serial.println(t2message.printMessage());
+    // t2message.printMessage();
+    // delay(5000);
 
-
+    boutonD.check();
+    countD = boutonD.getNumber();
+    // if (countD > 0) {
+    //    Serial.print("D"); 
+    //    Serial.println(countD);
+    // }
+    if (countD == 1) {
+        initialisation(); 
+    }
+    if (countD == 2) {
+        createChannel();
+    }
+    if (countD == 3) {
+        emitData();
+    }
 }
