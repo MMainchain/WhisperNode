66843c0fa05ec9cd14daa45964b401197eb984f1
Mael MAINCHAIN <mael@mainchain.fr>
2018-11-23 11:56:37 +0100

    Feat: comment code + TP3 ^Q10

--- src/main.cpp
+++ src/main.cpp
@@ -10,41 +10,39 @@
 
 // Element pins
 const int blueButton = 4;
-// const int yellowButton = 5;
 
+// Count of click
 int countD = 0;
-// int countG = 0;
 
+// Define button
 Bouton boutonD(blueButton);
-// Bouton boutonG(yellowButton);
 
-// T2Flash myflash;
-// RH_RF95 myradio;
-
+// Define message
 T2Message t2message;
+
+// Maaaaagic
 uint8_t radioBuf[(T2_MESSAGE_HEADERS_LEN + T2_MESSAGE_MAX_DATA_LEN)];
 
-int serialId = 10;
+// Serial id of WhisperNode
+const int serialId = 10;
+
+// Given id from remote server
 int id = 0;
 
+// Given id network
+int idNetwork = -1;
+
+// Given channel id from remote server
+int channelId;
+
+/**
+ * Setup function, first function to be called
+ */
 void setup() {
 
-    // put your setup code here, to run once:
-    Serial.begin(BAUD); // Définition du port de sortie 
+    Serial.begin(BAUD); // Définition out port 
 
-    /** TP2 **/
-
-    // Pour mémoire flash
-    // myflash.init(T2_WPN_FLASH_SPI_CS);
-    // myflash.powerDown();
-
-    // Pour puce LoRa
-    // myradio.init();
-    // myradio.sleep();
-
-    /** TP3 **/
     Wire.begin();
-
     LoRa.setPins(10,7,2); 
     if(!LoRa.begin(868E6)) { 
         Serial.println("Starting LoRa failed!"); 
@@ -54,24 +52,11 @@ void setup() {
     }
 }
 
-void createChannel() {
-    Serial.println("create channel");
-    while(1) {
-    int packetSize = LoRa.parsePacket();
-    // Serial.println(packetSize);
-        if (packetSize) {
-            int i = 0;
-            char *message;
-            while (LoRa.available()) {
-                message[i++] = (char) LoRa.read();
-            }
-            Serial.println(message);
-        }
-    }
-}
-
+/**
+ * Initialisation : called when 1 press to right button
+ */
 void initialisation(int idx, int src, int dst, int sdx, int cmd, const char *data, int len) {
-    Serial.println("Init");
+    Serial.println("Initializing...");
     uint8_t radioBufLen = 0;
 
     t2message.idx = idx;
@@ -81,7 +66,9 @@ void initialisation(int idx, int src, int dst, int sdx, int cmd, const char *dat
     t2message.sdx = sdx;
     t2message.cmd = cmd;
     t2message.len = len;
+
     memcpy(t2message.data, data, len);
+
     t2message.getSerializedMessage(radioBuf, &radioBufLen);
     LoRa.beginPacket(); 
     LoRa.write(radioBuf, radioBufLen);
@@ -89,7 +76,6 @@ void initialisation(int idx, int src, int dst, int sdx, int cmd, const char *dat
 
     char message[250];
     int idNode = 0;
-    int idNetwork = 0;
     int idWhisperNode = 0;
     
     while(idWhisperNode != serialId) {
@@ -112,25 +98,15 @@ void initialisation(int idx, int src, int dst, int sdx, int cmd, const char *dat
             id = idNode;
         }
     }
-
+    Serial.print("Initialized. Given id = ");
     Serial.println(id);
 }
 
-int read() {
-    int packetSize = LoRa.parsePacket();
-    if (packetSize) {
-        int i = 0;
-        char *message;
-        while (LoRa.available()) {
-            message[i++] = (char) LoRa.read();
-        }
-        Serial.println(message);
-    }
-}
-
-int emitData(int idx, int src, int dst, int sdx, int cmd, const char *data, int len) {
-    Serial.println("emit data");
-
+/**
+ * Create channel : called when 2 press to right button
+ */
+void createChannel(int idx, int src, int dst, int sdx, int cmd, const char *data, int len) {
+    Serial.println("Creating channel...");
     uint8_t radioBufLen = 0;
 
     t2message.idx = idx;
@@ -140,84 +116,86 @@ int emitData(int idx, int src, int dst, int sdx, int cmd, const char *data, int 
     t2message.sdx = sdx;
     t2message.cmd = cmd;
     t2message.len = len;
-    memcpy(t2message.data, data, len);
-    t2message.getSerializedMessage(radioBuf, &radioBufLen);
 
+    memcpy(t2message.data, data, len);
+
+    t2message.getSerializedMessage(radioBuf, &radioBufLen);
     LoRa.beginPacket(); 
     LoRa.write(radioBuf, radioBufLen);
     LoRa.endPacket();
 
+    char message[250];
+    int channel = -1;
+    int field = 0;
     
+    while(channel < 0) {
+        int i = 0;
+        int packetSize = LoRa.parsePacket();
+        if (packetSize) {
+            while (LoRa.available() && i < 250) {
+                message[i++] = (char) LoRa.read();
+            }
+            message[i++] = 0;
+        }
+
+        if (packetSize) {
+            t2message.setSerializedMessage((uint8_t*) message, i);
+
+            channel = atoi(strtok((char*)t2message.data, ";"));
+            field = atoi(strtok(NULL, ";"));
+
+            channelId = channel;
+        }
+    }
+
+    Serial.print("Channel id given = ");
+    Serial.println(channelId);
 }
 
+/**
+ * Send message : called when 3 press to right button
+ */
+int emitData(int idx, int src, int dst, int sdx, int cmd, const char *data, int len) {
+    Serial.println("Sending message...");
+    uint8_t radioBufLen = 0;
+
+    t2message.idx = idx;
+    t2message.src = src;
+    t2message.dst = dst;
+
+    t2message.sdx = sdx;
+    t2message.cmd = cmd;
+    t2message.len = len;
+
+    memcpy(t2message.data, data, len);
+
+    t2message.getSerializedMessage(radioBuf, &radioBufLen);
+    LoRa.beginPacket(); 
+    LoRa.write(radioBuf, radioBufLen);
+    LoRa.endPacket();
+
+    Serial.println("Message sent.");
+}
+
+/**
+ * Loop function, second function to be called
+ * Works like a while(true)
+ */
 void loop() {
-
-    /** TP1 **/
-    
-    // Bibliothèque Bouton    
-    // boutonD.check();
-    // countD = boutonD.getNumber();
-    // if (countD > 0) {
-    //    Serial.print("D"); 
-    //    Serial.println(countD);
-    //}
-
-    //boutonG.check();
-    //countG = boutonG.getNumber();
-    //if (countG > 0) {
-    //    Serial.print("G"); 
-    //    Serial.println(countG);
-    //}
-
-    /** TP2 **/
-    // LowPower.idle(SLEEP_FOREVER, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-    // LowPower.idle(SLEEP_FOREVER, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_ON);
-    // LowPower.idle(SLEEP_FOREVER, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_ON, TWI_OFF);
-    // LowPower.idle(SLEEP_FOREVER, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_ON, USART0_OFF, TWI_OFF);
-    // LowPower.idle(SLEEP_FOREVER, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_ON, USART0_ON, TWI_ON);
-
-    // LowPower.powerSave(SLEEP_FOREVER, ADC_OFF, BOD_OFF, TIMER2_OFF);
-    // LowPower.powerSave(SLEEP_FOREVER, ADC_ON, BOD_OFF, TIMER2_OFF);
-    // LowPower.powerSave(SLEEP_FOREVER, ADC_OFF, BOD_ON, TIMER2_OFF);
-    // LowPower.powerSave(SLEEP_FOREVER, ADC_OFF, BOD_OFF, TIMER2_ON);
-    // LowPower.powerSave(SLEEP_FOREVER, ADC_ON, BOD_ON, TIMER2_ON);
-
-    // LowPower.powerStandby(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
-    // LowPower.powerStandby(SLEEP_FOREVER, ADC_ON, BOD_OFF);
-    // LowPower.powerStandby(SLEEP_FOREVER, ADC_OFF, BOD_ON);
-    // LowPower.powerStandby(SLEEP_FOREVER, ADC_ON, BOD_ON);
-
-    // LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
-    // LowPower.powerDown(SLEEP_FOREVER, ADC_ON, BOD_OFF);
-    // LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_ON);
-    // LowPower.powerDown(SLEEP_FOREVER, ADC_ON, BOD_ON);
-
-    // Pour le calculateur
-    // myradio.init();
-    // digitalWrite(9, LOW);
-    // digitalWrite(6, HIGH);
-    // delay(500);
-    // digitalWrite(6, LOW);
-    // digitalWrite(9, HIGH);
-    // myradio.sleep();
-    // LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
-    // delay(5000);
-
-    /** TP3 **/
-    // Serial.println(t2message.printMessage());
-    // t2message.printMessage();
-    // delay(5000);
-
+    // Verify button D
     boutonD.check();
+    // Count number of press on it
     countD = boutonD.getNumber();
+    // Init if pressed one time
     if (countD == 1) {
         initialisation(0x00, 0x00, 0x01, 0x01, 0x00, "10;", 3);
     }
+    // Create channel if pressed two times
     if (countD == 2) {
-        createChannel();
+        createChannel(idNetwork, id, 0x01, 0x02, 0x00, "", 0);
     }
+    // Emit data if pressed three times
     if (countD == 3) {
-        emitData(0x00, 0x00, 0x01, 0x01, 0x00, "10;", 3);
+        emitData(idNetwork, id, 0x01, 0x03, 0x01, "Message;", 8);
     }
-    
 }
