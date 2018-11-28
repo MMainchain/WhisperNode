420cbad6d83b17b6a57614de5b8bc312bde622dc
Mael MAINCHAIN <mael@mainchain.fr>
2018-11-28 11:59:09 +0100

    Feat: Q6

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,8 @@ int myChannel =0;
 // Button
 const int button = 4;
 
+const int nbMin = 1; // Réveil toutes les nbMin minutes
+
 T2Flash myFlash;
 RTC_DS3231 rtc;      //we are using the DS3231 RTC
 
@@ -151,9 +153,15 @@ void setup() {
   // ...
   // Code a développer pour réveiller le WhisperNode toutes les secondes
   // ...
+  
+  
+  // Déclanchement toutes les secondes: 
   rtc.setAlarm(ALM1_EVERY_SECOND, 01, 00, 00, 00);
+  // Déclanchement toutes les minutes:
+  // rtc.setAlarm(ALM1_MATCH_MINUTES, rtc.now().second(), rtc.now().minute() + nbMin, 00, 00);
   rtc.alarmInterrupt(1, true);
-  attachInterrupt(1, WakeUpRTC, LOW);   
+  rtc.alarmInterrupt(2, true);
+  attachInterrupt(1, WakeUpRTC, LOW);
 }
 
 char message[255]; //buffeur de reception d'un message
@@ -244,8 +252,11 @@ void loop() {
   //protocoleReseau();
   //Serial.print(".");
 
+  // Déclanchement toutes les secondes (working): 
   rtc.setAlarm(ALM1_EVERY_SECOND, 01, 00, 00, 00);
-  attachInterrupt(1, WakeUpRTC, LOW); 
+  // Déclanchement toutes les minutes (not working):
+  //rtc.setAlarm(ALM1_MATCH_MINUTES, rtc.now().second(), rtc.now().minute() + nbMin, 00, 00);
+  attachInterrupt(1, WakeUpRTC, LOW);
 
   // Code a développer pour endormir le WhisperNode et le réveiller toutes les secondes
   myFlash.powerDown();
